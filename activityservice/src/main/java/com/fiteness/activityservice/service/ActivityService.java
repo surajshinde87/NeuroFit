3cebc38d11f8b745d1c3dcd2f94d7d665461010a
Suraj Shinde <surajpshinde8767@gmail.com>
2026-06-02 08:18:33 +0530

    Kafka Integrated

--- activityservice/src/main/java/com/fiteness/activityservice/service/ActivityService.java
+++ activityservice/src/main/java/com/fiteness/activityservice/service/ActivityService.java
@@ -5,6 +5,9 @@ import com.fiteness.activityservice.dto.ActivityResponse;
 import com.fiteness.activityservice.model.Activity;
 import com.fiteness.activityservice.repository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +16,10 @@ public class ActivityService {
 
     private  final ActivityRepository activityRepository;
     private final UserValidationService userValidationService;
+    private final KafkaTemplate<String, Activity> kafkaTemplate;
+
+    @Value("${kafka.topic.name}")
+    private String topicName;
 
     public ActivityResponse trackActivity(ActivityRequest request) {
 
@@ -31,6 +38,12 @@ public class ActivityService {
                 .additionalMetrics(request.getAdditionalMetrics())
                 .build();
         Activity savedActivity = activityRepository.save(activity);
+
+        try{
+            kafkaTemplate.send(topicName, savedActivity.getUserId(), savedActivity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return mapToResponse(savedActivity);
     }
